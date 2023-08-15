27aaf7950281ccc1a535cfdeea7af4d6a7dd4126
yeyun <hallo.yunye@gmail.com>
2023-08-15 22:25:02 +0900

    [ADD] 글 삭제 기능 구현, router.push와 refresh()

--- src/app/Controll.js
+++ src/app/Controll.js
@@ -1,10 +1,11 @@
 "use client";
 import Link from "next/link";
-import { useParams } from "next/navigation";
+import { useParams, useRouter } from "next/navigation";
 
 // 서버 컴포넌트에서 특정 부분만 클라이언트 컴포넌트로 사용하는 방법
 export function Controll() {
   const params = useParams();
+  const router = useRouter();
   const id = params.id;
 
   return (
@@ -19,7 +20,22 @@ export function Controll() {
             <Link href={"/update/" + id}>UPDATE</Link>
           </li>
           <li>
-            <input type="button" value="delete" />
+            <input
+              type="button"
+              value="delete"
+              onClick={() => {
+                const options = { method: "DELETE" };
+
+                fetch("http://localhost:9999/topics/" + id, options)
+                  .then((resp) => resp.json())
+                  .then((result) => {
+                    //router를 사용하여 홈으로 돌아가게 하기
+                    // refresh를 사용하여 삭제 사항 반영
+                    router.push("/");
+                    router.refresh();
+                  });
+              }}
+            />
           </li>
         </>
       ) : null}
